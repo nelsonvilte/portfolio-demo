8598acd7699ac47a56c85ffde624eb91705843b2
Nelson Vilte <nelsonvilte@gmail.com>
2022-02-02 01:20:01 -0300

    load information from firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,16 +10,33 @@ export class InfoPaginaService {
   info:InfoPagina ={};
   cargada = false;
 
+  equipo: any[]=[];
+
   constructor(private http: HttpClient) { 
     //console.log("Servicio de infopagina listo");
     //leer archivo json
+   this.cargarInfo();
+   this.cargarEquipo();
+
+  }
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
     .subscribe((resp:InfoPagina) =>{
 
       this.cargada=true;
       this.info= resp;
+    });
+
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://template-hmtl-angular-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe( ( resp: any) => {
+
+      this.equipo= resp;
       console.log(resp);
     });
 
   }
+
 }
